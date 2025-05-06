68967e57f8cc81c2449cadf29c1f68d209a81435
Sean <seanasb52058@gmail.com>
2025-05-06 10:05:24 -0500

    Limited this assignment to workable basis till I can understand other material

--- HotelReservations/src/main/java/com/pluralsight/Main.java
+++ HotelReservations/src/main/java/com/pluralsight/Main.java
@@ -2,30 +2,24 @@ package com.pluralsight;
 
 import com.pluralsight.objects.Reservation;
 import com.pluralsight.objects.Room;
+import com.pluralsight.objects.UserEmployee;
+import com.pluralsight.ui.LogInScreen;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
+
     private LocalDate startDate = null;
     private LocalDate endDate = null;
 
-    public static void main(String[] args) {
-        Room newRoom = new Room("king", 1, false, false, true);
-        Reservation newStay = new Reservation("Sean",3, false, newRoom);
-        newRoom.checkIn();
-        newRoom.checkOut();
-        newRoom.cleanRoom();
-        System.out.println(newRoom.toString());
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
     }
 
-    public static void getDates () {
-
-    }
-    public LocalDate setStartDate (LocalDate startDate) {
-
-        return this.startDate = startDate;
-    }
 
 }
